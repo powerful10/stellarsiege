9a3031ddfbd63a2628c7475eb92b307aef21350f
Oybek <oa8315980@gmail.com>
2026-02-06 10:15:37 +0500

    Enable Firebase web config

--- public/game/firebase-config.js
+++ public/game/firebase-config.js
@@ -19,7 +19,15 @@
 // IMPORTANT: For Google sign-in you must run this game on http://localhost (or HTTPS hosting).
 // Opening index.html via file:// will not work with OAuth popups.
 
-window.FIREBASE_CONFIG = null;
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAHzFAigw7IS__GSdoHlqPelxcO1BFnk5A",
+  authDomain: "stellar-siege.firebaseapp.com",
+  databaseURL: "https://stellar-siege-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "stellar-siege",
+  storageBucket: "stellar-siege.firebasestorage.app",
+  messagingSenderId: "1058826698690",
+  appId: "1:1058826698690:web:a8483d043092ceaa28a05b",
+};
 
 // Optional: payments backend (Lemon Squeezy) base URL (same-origin if empty).
 // When you deploy the backend, set e.g.:
